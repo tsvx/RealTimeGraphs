f85c2030361dea150effbf4daa78468679fcae23
tsul <tsul@mail.ru>
2016-12-19 20:06:34 +0300

    Added what to be a user-painted control.

--- RtwfApp/Form1.Designer.cs
+++ RtwfApp/Form1.Designer.cs
@@ -28,13 +28,24 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.movingGraphControl1 = new RtwfApp.MovingGraphControl();
 			this.SuspendLayout();
+			// 
+			// movingGraphControl1
+			// 
+			this.movingGraphControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.movingGraphControl1.Location = new System.Drawing.Point(0, 0);
+			this.movingGraphControl1.Name = "movingGraphControl1";
+			this.movingGraphControl1.Size = new System.Drawing.Size(945, 449);
+			this.movingGraphControl1.TabIndex = 0;
+			this.movingGraphControl1.Text = "movingGraphControl1";
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(945, 449);
+			this.Controls.Add(this.movingGraphControl1);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.ResumeLayout(false);
@@ -42,6 +53,8 @@
 		}
 
 		#endregion
+
+		private MovingGraphControl movingGraphControl1;
 	}
 }
 
---
+++ RtwfApp/MovingGraphControl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RtwfApp
+{
+	public partial class MovingGraphControl : Control
+	{
+		public MovingGraphControl()
+		{
+			InitializeComponent();
+			this.SetStyle(
+				ControlStyles.OptimizedDoubleBuffer |
+				ControlStyles.Opaque |
+				ControlStyles.ResizeRedraw |
+				ControlStyles.AllPaintingInWmPaint |
+				ControlStyles.UserPaint,
+				true);
+			this.BackColor = Color.DarkGreen;
+		}
+
+		protected override void OnPaint(PaintEventArgs pe)
+		{
+			base.OnPaint(pe);
+		}
+	}
+}

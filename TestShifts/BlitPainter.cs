11a01c602247cb771c5992532fb069d6c751d723
tsul <tsul@mail.ru>
2017-02-07 20:13:26 +0300

    !Forget to add files themselves.

---
+++ TestShifts/BlitPainter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestShifts
+{
+	class BlitPainter : IPainter
+	{
+		public void Dispose()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
